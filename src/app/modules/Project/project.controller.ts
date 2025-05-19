072d510728f2a2f5889b5b8d5b7fc1b81699cb5b
rifat-sarker <rifatswd@gmail.com>
2025-05-20 02:36:00 +0600

    Add models for Project, Blog, Skill and implement project controller and service

--- src/app/modules/Project/project.controller.ts
+++ src/app/modules/Project/project.controller.ts
@@ -1,1 +1,17 @@
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
 
+const createProject = catchAsync(async (req, res) => {
+  const data = req.body;
+  
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Project created successfully",
+    data: data,
+  });
+});
+
+export const ProjectController = {
+  createProject,
+};
---
+++ src/app/modules/Project/project.service.ts
@@ -0,0 +1,7 @@
+const createProject = async () => {
+    const result = 
+};
+   
+export const ProjectService = {
+  createProject,
+};
